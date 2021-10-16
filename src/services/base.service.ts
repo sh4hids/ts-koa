cc42194044d1a1010fe39996ec50850674ad9c7b
Shahidul Islam Majumder <dev@shahid.pro>
2021-10-16 20:26:57 +0600

    ➕ add lessons about base service

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -13,4 +13,13 @@ export class BaseService<T> {
   async getById(id: number): Promise<T> {
     return this.repo.findOneOrFail(id)
   }
+
+  async update(id: number, body: DeepPartial<T>): Promise<T>{
+    await this.repo.update(id, body);
+    return this.getById(id);
+  }
+
+  async delete(id: number): Promise<T> {
+    return this.delete(id)
+  }
 }
